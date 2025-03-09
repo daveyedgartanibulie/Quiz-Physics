59b92724428019d6a3e4e6844583b6e1cf0b1ffc
daveyedgar <105902240+daveyedgartanibulie@users.noreply.github.com>
2025-03-09 21:25:55 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,200 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Seorang pengamat di bumi melihat dua pesawat antariksa A dan B yang bergerak berlawanan arah mendekati bumi dengan kelajuan masing-masing 0,5c dan 0,6c. Maka, berapa kelajuan pesawat A menurut pilot di pesawat B dan berapa kelajuan pesawat B menurut pilot di pesawat A.",
+        answer: "A. 0,85c",
+        options: [
+            "A. 0,85c",
+            "B. 0,5c",
+            "C. 0,75c",
+            "D. 0,65c"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Anton berada di bumi, sedangkan Bernard naik pesawat antariksa yang bergerak dengan kecepatan 0,8c pergi pulang terhadap bumi. Apabila Anton mencatat kepergian Bernard selama 20 tahun, maka berapa lama Bernard mencatat kepergiannya mengendarai pesawat tersebut?",
+        answer: "B. 12 tahun",
+        options: [
+            "A. 11 tahun",
+            "B. 12 tahun",
+            "C. 10 tahun",
+            "D. 20 tahun"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Sebuah partikel yang bergerak dengan kelajuan 0,3c terhadap kerangka acuan laboratorium memancarkan elektron searah dengan kecepatan 0,3c relatif terhadap partikel. Tentukan laju elektron tersebut menurut kerangka acuan laboratorium!",
+        answer: "A. 0,31c",
+        options: [
+            "A. 0,31c",
+            "B. 0,30c",
+            "C. 0,29c",
+            "D. 0,51c"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Sumber cahaya A berada di bumi dan mengirim isyarat tiap 12 menit. Pengamat B berada dalam pesawat antariksa yang meninggalkan bumi dengan kecepatan 0,6c terhadap bumi. Berapa selang waktu pengamat B menerima isyarat-isyarat dari A?",
+        answer: "B. 15 menit",
+        options: [
+            "A. 20 menit",
+            "B. 15 menit",
+            "C. 25 menit",
+            "D. 35 menit"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Jarak dua kota di Bumi adalah 800 km. Berapakah jarak kedua kota tersebut bila diukur dari sebuah pesawat antariksa yang terbang dengan kecepatan 0,6c searah kedua kota?",
+        answer: "D. 640 km",
+        options: [
+            "A. 670 km",
+            "B. 60 km",
+            "C. 630 km",
+            "D. 640 km"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Seberkas Cahaya dengan energi 10,09 eV datang pada permukaan logam denga fungsi kerja 3,338 eV, energi kinetic maksimum electron yang dihasilkan adalah... eV",
+        answer: "A. 6,74",
+        options: [
+            "A. 6,74",
+            "B. 2,28",
+            "C. 4,56",
+            "D. 8,85"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Permukaan sebuah logam memiliki frekuensi ambang 1, 5 * 10 ^ 16 * Hz disinari Cahaya dengan frekuensi 2 * 10 ^ (16Hz) jika h = 6, 6x 10-Js maka besar energi kinetic yang dimiliki oleh electron yang terlepas dari permukaan logam adalag .... ev",
+        answer: "D. 20,6",
+        options: [
+            "A. 4,12",
+            "B. 29,6",
+            "C. 13,6",
+            "D. 20,6"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Pada peristiwa efek compton sebuah partikel ditumbuk oleh sebeerkas sinar dengan panjang gelombang 100 nm sehingga hamburan cahaya membentuk sudut 37°terhadap arah horizontal dengan panjang gelombang hamburan 400 nm, jika sinar yang sama ditembakkan pada partikel yang lain sehinggan hambunran sinar membentuk sudut 53º maka Panjang gelombang hamburan sinarnya adalah....nm.",
+        answer: "A. 700",
+        options: [
+            "A. 700",
+            "B. 100",
+            "C. 200",
+            "D. 500"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Seberkas sinar dengan panjang gelombang 2, 43 * 10 ^ - 11 * m mengalami hamburan dengan sudut 37 setelah menabrak sebuah partikel, jika tan 37°=3/4 maka perubahan Panjang gelombang foton adalah .... 10-14m",
+        answer: "B. 48,6",
+        options: [
+            "A. 72,9",
+            "B. 48,6",
+            "C. 24,3",
+            "D. 2,43"
+        ]
+    },
+    {
+        numb: 10,
+        question: "11011011(2)=.......(16)",
+        answer: "A. DB(16)",
+        options: [
+            "A. DB(16)",
+            "B. DD(16)",
+            "C. DE(16)",
+            "D. DC(16)"
+        ]
+    },
+    {
+        numb: 11,
+        question: "6789(10)=.......(16)",
+        answer: "A. 1A85(16)",
+        options: [
+            "A. 1A85(16)",
+            "B. 1BBB5(16)",
+            "C. 1CBB5(16)",
+            "D. 1DCC5(16)"
+        ]
+    },
+    {
+        numb: 12,
+        question: "F2C(16)=.......(2)",
+        answer: "A. 111100101100(2)",
+        options: [
+            "A. 111100101100(2)",
+            "B. 111100101101(2)",
+            "C. 111100101110(2)",
+            "D. 111100101111(2)"
+        ]
+    },
+    {
+        numb: 13,
+        question: "8A7(16)+DBA(16)=.......(16)",
+        answer: "C. 1631(16)",
+        options: [
+            "A. 1361(16)",
+            "B. 1451(16)",
+            "C. 1631(16)",
+            "D. 1731(16)"
+        ]
+    },
+    {
+        numb: 14,
+        question: "F9C(16)+ DA1(16)=.......(16)",
+        answer: "A. 1D30(16)",
+        options: [
+            "A. 1D30(16)",
+            "B. 1E30(16)",
+            "C. 1F30(16)",
+            "D. 1C30(16)"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Perhatikan reaksi inti berikut! (C) 7N14 + X → 8O17 + 1H1 Pada reaksi di atas X adalah ….",
+        answer: "C. partikel alfa",
+        options: [
+            "A. proton",
+            "B. elektron",
+            "C. partikel alfa",
+            "D. deutron"
+        ]
+    },
+    {
+        numb: 16,
+        question: "Inti atom yang terbentuk memenuhi reaksi fusi berikut ini: 1H1 + 1H1 → 1d2 + 1e0 + E Diketahui: massa 1H1 = 1,00780 sma massa 1d2  = 2,01410 sma massa 1e0  = 0,00055 sma 1 sma = 931 MeV Nilai E (energi yang dihasilkan) pada reaksi fusi tersebut adalah ….",
+        answer: "A. 1,47 MeV",
+        options: [
+            "A. 1,47 MeV",
+            "B. 1,02 MeV",
+            "C. 0,98 MeV",
+            "D. 0,88 MeV"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Diketahui sebuah massa inti atom Rumus Energi Ikat Inti dan Contoh Soal - Materi Fisika Kelas 12 43 adalah 9,0121 MeV, massa proton 1,0078 sma, massa neutron 1,0087 sma. Maka, berapakah energi ikat dari atom tersebut jika diketahui 1 sma = 931 MeV?",
+        answer: "A. 58,28 MeV",
+        options: [
+            "A. 58,28 MeV",
+            "B. 58,2 MeV",
+            "C. 58,8 MeV",
+            "D. 58,29 MeV"
+        ]
+    },
+    {
+        numb: 18,
+        question: "Elektron dari sebuah atom hidrogen mengalami relaksasi dari tingkat energi n=4 ke tingkat energi n=2 dengan melepaskan sejumlah energi dalam bentuk gelombang elektromagnetik. Panjang gelombang yang dilepaskan elektron tersebut adalah .... (R = 1,097x107 m²¹) ",
+        answer: "D. 500 nm",
+        options: [
+            "A. 200 nm",
+            "B. 300 nm",
+            "C. 400 nm",
+            "D. 500 nm"
+        ]
+    }
+];
